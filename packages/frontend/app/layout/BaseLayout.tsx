b5d633cb140e77b27a1a42bcc1459fe26e2e10df
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2025-07-14 10:15:16 +0200

    [feat] Add base layout to all routes

--- packages/frontend/app/layout/BaseLayout.tsx
+++ packages/frontend/app/layout/BaseLayout.tsx
@@ -2,8 +2,13 @@ import { Outlet } from 'react-router';
 
 export default function BaseLayout() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <header>header</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>footer</footer>
+    </>
+
   );
 }
--- packages/frontend/app/routes.ts
+++ packages/frontend/app/routes.ts
@@ -1,8 +1,14 @@
 import {
   type RouteConfig,
   index,
+  layout,
 } from "@react-router/dev/routes";
 
 export default [
-  index('routes/home.tsx'),
+  layout(
+    './layout/BaseLayout.tsx',
+    [
+      index('routes/home.tsx'),
+    ],
+  ),
 ] satisfies RouteConfig;
